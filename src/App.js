01e4e802b2ab25e1b634b1594711eb1e4ebeb6d0
Carlos Angulo Leandro <carlosanguloleandro@gmail.com>
2023-11-03 00:17:32 -0600

    adding graphql

--- src/App.js
+++ src/App.js
@@ -3,17 +3,9 @@ import React from "react";
 import ChildScreenshots from "./Screenshots";
 
 function App() {
-  // URLs reales de thumbnails
-  const realChildScreenshots = [
-    { id: 1, thumbnail: "https://picsum.photos/200/300/?random" },
-    { id: 2, thumbnail: "https://picsum.photos/200/300/?random" },
-    { id: 3, thumbnail: "https://picsum.photos/200/300/?random" },
-    // Agrega tus propias URLs de imágenes reales
-  ];
-
   return (
     <div className="App">
-      <ChildScreenshots screenshots={realChildScreenshots} />
+      <ChildScreenshots />
     </div>
   );
 }
--- src/Screenshots.js
+++ src/Screenshots.js
@@ -1,10 +1,52 @@
 import React from "react";
+import { ApolloClient, InMemoryCache, useQuery, gql } from "@apollo/client";
 import "./Screenshots.css";
 
-const ChildScreenshots = ({ screenshots }) => {
+const mockChildScreenshots = [
+  { id: 1, thumbnail: "https://picsum.photos/200/300/?random" },
+  { id: 2, thumbnail: "https://picsum.photos/200/300/?random" },
+  { id: 3, thumbnail: "https://picsum.photos/200/300/?random" },
+];
+
+const cache = new InMemoryCache();
+
+cache.writeQuery({
+  query: gql`
+    query {
+      childScreenshots {
+        id
+        thumbnail
+      }
+    }
+  `,
+  data: {
+    childScreenshots: mockChildScreenshots,
+  },
+});
+
+const client = new ApolloClient({
+  uri: "https://fakeGraphQLURL.com",
+  cache: cache,
+});
+
+const GET_CHILD_SCREENSHOTS = gql`
+  query {
+    childScreenshots {
+      id
+      thumbnail
+    }
+  }
+`;
+
+const ChildScreenshots = () => {
+  const { loading, error, data } = useQuery(GET_CHILD_SCREENSHOTS, { client });
+
+  if (loading) return <p>loading...</p>;
+  if (error) return <p>Error</p>;
+
   return (
     <div className="screenshot-list">
-      {screenshots.map((screenshot) => (
+      {data.childScreenshots.map((screenshot) => (
         <div key={screenshot.id} className="screenshot-card">
           <img src={screenshot.thumbnail} alt={`Screenshot ${screenshot.id}`} />
         </div>
